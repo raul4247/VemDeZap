26715d8a44dad79f8756c6decd7f18f59528a357
Raul F. Mansur <raulfmansur@hotmail.com>
2018-05-04 02:47:16 -0300

    attempt to fix the footer bug

--- components/footer.php
+++ components/footer.php
@@ -3,8 +3,7 @@ echo '<footer class="footer">
 		<div class="container">
 			<div class="content has-text-centered">
 				<p>Chama os criadores no zap:</p>
-				<p>😉 Raul Mansur</p>
-				<p>😘 Lucas Campregher</p>
+				<p>😉 Raul Mansur 😘 Lucas Campregher</p>
 			</div>
 		</div>
 	</footer>';
