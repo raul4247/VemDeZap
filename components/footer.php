e62733bd998ee111d5ad2bc264754efa13cf96b3
Raul Mansur <raulfmansur@hotmail.com>
2018-05-08 23:21:29 -0300

    Link para instagram dos criadores

--- components/footer.php
+++ components/footer.php
@@ -3,7 +3,10 @@ echo '<div class="footer" >
 		<div class="container">
 			<div class="content has-text-centered">
 				<p>Chama os criadores no zap:</p>
-				<p>😉 Raul Mansur 😘 Lucas Campregher</p>
+				<p>
+					😉 <a target="_blank" href="https://www.instagram.com/raulfmansur/">@raulfmansur</a> 
+					😘 <a target="_blank" href="https://www.instagram.com/guimm4/">@guimm4</a>
+				   </p>
 			</div>
 		</div>
 	</div>';
