7600404f79eaf99208b0b3b9f3bb0db1091f3ef0
Raul Mansur <raulfmansur@hotmail.com>
2018-05-08 23:08:16 -0300

    Javascript no index.php organizado

--- index.php
+++ index.php
@@ -11,32 +11,18 @@
   <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 
-  <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
-
-
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118709061-1"></script>
   <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-118709061-1');
+   
   </script>
-
-
-  
 </head>
 <body>
-
   <?php require("components/header.php"); ?>
-  
   <div class="container">
     <div id="cardForm" class="content">
       <div class="card">
         <div class="card-content">
           <h2>Insira seu número:</h2>
           <form name="form" action="message.php" method="get">
-
             <div class="field">
               <div class="control">
                 <input class="input telInput" type="tel" name="num" placeholder="(xx) xxxxx - xxxx">
@@ -48,11 +34,11 @@
       </div>
     </div>
   </div>
-
   <?php require("components/footer.php"); ?>
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script> <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js"></script>          
-<script type="text/javascript" src="js/materialize.min.js"></script>
-<script type="text/javascript" src="js/use.js"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-118709061-1"></script>
+<script type="text/javascript" src="js/index.js"></script>
 </html>
